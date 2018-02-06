9f84d1b5364f5785f46661a2f9576c2ed5388417
riarangogi <riarangogi@gmail.com>
2018-02-06 01:44:55 -0500

    I found the num_doc negatives in cclientes variable

--- Prueba1/script analisis exploratorio.R
+++ Prueba1/script analisis exploratorio.R
@@ -25,3 +25,24 @@ table(dispositivos_na) #No hay valores faltantes
 
 clientes_na<-complete.cases(data_clientes)
 table(clientes_na) #No hay valores faltantes
+
+##4 Buscamos valores anomalos tales como id o codigos negativos
+min_codigo_barrios<-min(data_barrios$codigo)
+min_codigo_barrios #El minimo es positivo, con lo que nos aseguramos que
+# ninguno es negativo
+
+min_codigo_dispositivos<-min(data_dispositivos$codigo)
+min_codigo_dispositivos#El minimo es positivo, con lo que nos aseguramos que
+# ninguno es negativo
+
+min_idbarrio_dispositivos<-min(data_dispositivos$id_barrio)
+min_idbarrio_dispositivos#El minimo es positivo, con lo que nos aseguramos que
+# ninguno es negativo
+
+min_coddispositivo_clientes<-min(data_clientes$cod_dispositivo)
+min_coddispositivo_clientes#El minimo es positivo, con lo que nos aseguramos
+#que ninguno es negativo
+
+min_numdoc_clientes<-min(data_clientes$num_doc)
+min_numdoc_clientes#El minimo es negativo, con lo que nos aseguramos que
+# almenos uno es negativo
