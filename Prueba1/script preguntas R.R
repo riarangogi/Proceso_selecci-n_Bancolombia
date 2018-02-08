2c1f653e14a6d415c530583f0cbc3ff68839ed57
riarangogi <riarangogi@gmail.com>
2018-02-08 00:17:28 -0500

    I answered the second question to R lenguage

--- Prueba1/script preguntas R.R
+++ Prueba1/script preguntas R.R
@@ -1,6 +1,10 @@
 library(data.table)
 library(plyr)
 
+
+##1·¿Cuales son las zonas más concurridas de la ciudad ?.
+
+
 #Leer archilvos
 data_barrios<-read.csv('./Datos Limpios/barrios_tidy.csv',header = TRUE,
                         sep = ',',stringsAsFactors = FALSE)
@@ -47,3 +51,27 @@ bar_cli<-aggregate(num_trx~nombre_barrio,bar_cli,FUN = sum)
 
 #ordenar los barrios de mayor a meenor segun el número de trx
 bar_cli<-bar_cli[order(bar_cli$num_trx,decreasing = TRUE),]
+
+
+##2 ¿Cuántos dispositivos y de que tipo se encuentran en cada zona
+## de la ciudad?
+
+#Leer archilvos
+data_barrios<-read.csv('./Datos Limpios/barrios_tidy.csv',header = TRUE,
+                       sep = ',',stringsAsFactors = FALSE)
+
+data_dispositivos<-read.csv('./Datos Limpios/dispositivos_tidy.csv',
+                            header = TRUE, sep = ',',stringsAsFactors = FALSE)
+
+#hacer un inner join 
+bar_disp<-join(table_barrios,table_dispositivos,type='inner')
+
+#mostrar solo las columnas id_disp y nombre_barrio
+bar_disp<-bar_disp[,2:4]
+
+#cambiamos la columna id_disp por un vector de unos, pues solo nos intereza
+# saber que hay un dispositivo
+bar_disp$id_disp<-rep(1,length(bar_disp$nombre_barrio))
+
+#contamos los dispositivos presentes en cada barrio respecto a su tipo
+bar_disp_tipo<-aggregate(id_disp~nombre_barrio+tipo_disp,bar_disp,FUN = sum)
