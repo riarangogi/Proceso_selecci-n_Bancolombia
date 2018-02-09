9e3645dfa761f5772a6cadef028415cdb0cd9be9
riarangogi <riarangogi@gmail.com>
2018-02-08 19:08:28 -0500

    I created the python model file

---
+++ Prueba2/modelo arb_dec.py
@@ -0,0 +1,34 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.tree import export_graphviz
+import graphviz
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+"""
+data=
+
+X_ent,y_ent,X_test,y_test = train_test_split()
+
+arbol=DecisionTreeClassifier()
+
+arbol.fit(X_ent,y_ent)
+
+arbol.score(X_test,y_test)
+
+arbol.score(X_ent,y_ent)
+
+export_graphviz(arbol,out_file='arbol.dot',class_names=,feature_names=,impurity=False,filled=True)
+
+with open('arbol.dot') as f:
+	dot_graph=f.read()
+graphviz.Source(dot_graph)
+
+caract=
+plt.barh=
+plt.yticks=
+plt.xlabel=
+plt.ylabel=
+plt.show()"""
+
